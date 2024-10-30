7f8d579cf960f89ea54903984b73784fe1893e38
Cbstray <calebstraily01@gmail.com>
2024-10-30 14:57:57 -0500

    Update Arraymap.js

--- src/components/Arraymap.js
+++ src/components/Arraymap.js
@@ -1,5 +1,30 @@
 import React from "react";
 
+const fakeData =
+[
+    {
+        fname: "Scott",
+        lname: "Syverson",
+        transportation: "car",
+        age: 50,
+        old: false
+    },
+    {
+        fname: "Nicole",
+        lname: "Torres",
+        transportation: "car",
+        age: 32,
+        old: false
+    },
+    {
+        fname: "Caleb",
+        lname: "Straily",
+        transportation: "home",
+        age: 24,
+        old: true
+    }
+]
+
 function Arraymap()
 {
 
@@ -21,12 +46,35 @@ function Arraymap()
     const capitalize = names.map(
         (name) => 
         {
-            return name.charAt(0).toUpperCase()
+            let bruh = name.charAt(0).toUpperCase()
             + name.substring(1);
+
+            return <p>{bruh}</p>;
         }
     )
 
     console.log (capitalize);
+
+    const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
+
+    const pokemonPTags = pokemon.map(
+        (mon) => 
+        {
+            return <p>{mon}</p>
+        }
+    )
+
+    const listComponents = fakeData.map(
+        (currentItem) =>
+        {
+            return <TempItem 
+            firstName={currentItem.fname} 
+            lastName={currentItem.lname} 
+            transportation={currentItem.transportation} 
+            ageNum={currentItem.age} 
+            bool={currentItem.old} />
+        }
+    )
 
     return(
         <div className="row">
@@ -34,8 +82,24 @@ function Arraymap()
                 <h1>Array Mapping Section!</h1>
             </div>
             <div className="col-12">
-                {capitalize};
+                {capitalize}
             </div>
+            <div className="col-12">
+                {pokemonPTags}
+            </div>
+            {listComponents}
+        </div>
+    )
+}
+
+function TempItem(props)
+{
+    return (
+        <div className="col-3">
+            <p>First Name: {props.firstName} Last Name: {props.lastName} </p>
+            <p>Transportation: {props.transportation} </p>
+            <p>Age: {props.ageNum}</p>
+            <p>Old Status: {props.bool}</p>
         </div>
     )
 }
